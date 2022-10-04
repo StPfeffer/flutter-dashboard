c3b4addbc1f113eaf5b171bb879d01ffbec4f0c8
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2022-10-04 15:49:25 -0300

    Create main_screen.dart

---
+++ dashboard/lib/screens/main/main_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'components/side_menu.dart';
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          children: [
+            Expanded(
+              // default flex = 1
+              // it takes 1/6 part of the screen
+              child: SideMenu(),
+            ),
+            Expanded(
+              // it takes 5/6 part of the screen
+              flex: 5,
+              child: Container(
+                color: Colors.blue,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
