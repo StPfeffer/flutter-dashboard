a6a97255ee771e72a43cc9358497dd9019c357ea
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2022-10-09 14:58:01 -0300

    Added light theme colors

--- dashboard/lib/constants.dart
+++ dashboard/lib/constants.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
-const primaryColor = Color(0xFF2697FF);
-const secondaryColor = Color(0xFF2A2D3E);
-const bgColor = Color(0xFF212332);
+const darkPrimaryColor = Color(0xFF2697FF);
+const darkSecondaryColor = Color(0xFF2A2D3E);
+const darkBgColor = Color(0xFF212332);
+const lightPrimaryColor = Color(0xFF62929E);
+const lightSecondaryColor = Color(0xFF95A78D);
+const lightBgColor = Color(0xFFF0F4EF);
 const defaultPadding = 16.0;
