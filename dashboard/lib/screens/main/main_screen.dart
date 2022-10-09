e8f6445f77bc5b76e9b844502f0e7063f6d91329
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2022-10-09 15:01:46 -0300

    Update main_screen.dart

--- dashboard/lib/screens/main/main_screen.dart
+++ dashboard/lib/screens/main/main_screen.dart
@@ -1,20 +1,27 @@
+import 'package:dashboard/controllers/menu_controller.dart';
+import 'package:dashboard/responsive.dart';
 import 'package:flutter/material.dart';
 import '../dashbord/dashboard_screen.dart';
 import 'components/side_menu.dart';
+import 'package:provider/provider.dart';
 
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: context.read<MenuController>().scaffoldKey,
+      drawer: SideMenu(),
       body: SafeArea(
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start, // header to top
           children: [
-            Expanded(
-              // default flex = 1
-              // it takes 1/6 part of the screen
-              child: SideMenu(),
-            ),
+            // Side menu only for large screens
+            if (Responsive.isDesktop(context))
+              Expanded(
+                // default flex = 1
+                // it takes 1/6 part of the screen
+                child: SideMenu(),
+              ),
             Expanded(
               // it takes 5/6 part of the screen
               flex: 5,
